43a5e2e6e1782fb24dfd93d723a250ad62c42ba1
Iria Tam <iria@smugmug.com>
2014-10-09 13:55:31 -0700

    sum of integers from file in javascript

---
+++ sumOfIntegersFromFile/sumOfIntegersFromFile.js
@@ -0,0 +1,10 @@
+var fs  = require("fs");
+var sum = 0;
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        sum += parseInt(line);
+    }
+});
+
+console.log(sum);
